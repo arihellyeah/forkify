51f9871246265d6429391ba1820f09a10a1ce29c
Arielle Loring <arieloring@gmail.com>
2020-02-17 11:41:08 -0700

    add shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
+
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 
@@ -121,3 +123,5 @@ elements.recipe.addEventListener('click', e => {
 
   }
 });
+
+window.l = new List();
